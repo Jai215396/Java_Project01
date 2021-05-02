fb087f8ec294e64371c8f0a8aa80ff82e8bc988c
Jaideep kumar <41812727+Jai215396@users.noreply.github.com>
2021-05-03 03:04:14 +0530

    Update Python01.py

--- Python01.py
+++ Python01.py
@@ -1,5 +1,5 @@
 #--- Sample Python Code
 # This program prints Hello, world!
-print ("Hello Jaideep Kumar 01")
-print ("Hello Jaideep Kumar 02")
-print ("Hello Jaideep Kumar 03")
+print ('Hello Jaideep Kumar 01')
+print ('Hello Jaideep Kumar 02')
+print ('Hello Jaideep Kumar 03')
