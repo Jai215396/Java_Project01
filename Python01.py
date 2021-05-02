72d868e9ef678f15e9f6662698ba8e74b34912dc
Jaideep kumar <41812727+Jai215396@users.noreply.github.com>
2021-05-03 02:35:48 +0530

    Update Python01.py

--- Python01.py
+++ Python01.py
@@ -1,11 +1,5 @@
 #--- Sample Python Code
-num1 = 1.5
-num2 = 6.3
-
-print ('Num1 = ', num1)
-print ('Num2 = ', num2)
-# Add two numbers
-sum = num1 + num2
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+# This program prints Hello, world!
+print('Hello, world! 01')
+print('Hello, world! 02')
+print('Hello, world! 03')
